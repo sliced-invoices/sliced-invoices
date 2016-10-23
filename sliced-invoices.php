4c35926f0d9029a4e0bf985269de1f82163922c0
David Grant <support@slicedinvoices.com>
2016-10-23 14:44:29 -0700

    v3.1.1

--- sliced-invoices.php
+++ sliced-invoices.php
@@ -5,7 +5,7 @@
  * Plugin Name:       Sliced Invoices
  * Plugin URI:        http://slicedinvoices.com/
  * Description:       Create professional Quotes & Invoices that clients can pay for online.
- * Version:           3.1.0
+ * Version:           3.1.1
  * Author:            Sliced Invoices
  * Author URI:        http://slicedinvoices.com/
  * Text Domain:       sliced-invoices
@@ -16,7 +16,7 @@ if ( ! defined('ABSPATH') ) {
 	exit;
 }
 
-define( 'SLICED_VERSION', '3.1.0' );
+define( 'SLICED_VERSION', '3.1.1' );
 define( 'SLICED_DB_VERSION', '3' );
 define( 'SLICED_PATH', plugin_dir_path( __FILE__ ) );
 
