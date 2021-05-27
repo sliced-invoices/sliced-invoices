fdcaca00570d4bcf356569be9d34c0a2f4af70db
David Grant <support@slicedinvoices.com>
2021-05-26 20:29:40 -0700

    clear ALL admin notices upon plugin deactivation (some like invoice_viewed and quote_viewed were not being cleared)

--- core/class-sliced-deactivator.php
+++ core/class-sliced-deactivator.php
@@ -13,19 +13,27 @@ if ( ! defined('ABSPATH') ) { exit; }
 class Sliced_Deactivator {
 
 	/**
-	 * Short Description. (use period)
+	 * Deactivation tasks.
 	 *
-	 * Long Description.
+	 * Things to do when the Sliced Invoices plugin is deactivated.
 	 *
+	 * @version 3.?.?
 	 * @since   2.0.0
 	 */
 	public static function deactivate() {
+		
+		global $wpdb;
 	
-		// Sliced Recurring Tasks
+		// clear recurring tasks
 		wp_clear_scheduled_hook( 'sliced_invoices_hourly_tasks' );
-
+		
+		// flush rewrite rules
 		flush_rewrite_rules();
-
+		
+		// clear admin notices
+		delete_option( 'sliced_admin_notices' );
+		$wpdb->get_results( "DELETE FROM {$wpdb->options} WHERE option_name LIKE 'sliced_admin_notice_%'" );
+		
 	}
 
 }
