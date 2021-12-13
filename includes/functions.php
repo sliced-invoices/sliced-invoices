21ce116ab20e536dc3c9910a8bff5386a1f60b4a
David Grant <support@slicedinvoices.com>
2021-12-12 21:25:34 -0800

    add discount and tax args to quote/invoice creation functions

--- includes/functions.php
+++ includes/functions.php
@@ -10,29 +10,35 @@ if ( ! defined( 'ABSPATH' ) ) {
  * all others are optional.  Example $args:
  *
  *     $args = array(
- *         'title'           => 'Invoice for John Doe',
  *
- *         'description'     => 'Description of work to be provided to John Doe.',
+ *                           // REQUIRED. the WordPress User ID corresponding to the Sliced Client record.
+ *         'client_id'       => 123,
  *
- *         'client_id'       => 123, // REQUIRED. the WordPress User ID corresponding to the Sliced Client record.
+ *                           // Optional. The WordPress User ID of the person creating the invoice. Defaults to the current user.
+ *         'creator_user_id' => 1,
  *
- *         'creator_user_id' => 1,   // the WordPress User ID of the person creating the invoice.
- *
- *                           // UNIX timestamp to be used for invoice date. If not provided, defaults to now:
+ *                           // Optional. UNIX timestamp to be used for invoice date. Defaults to current time.
  *         'created_at'      => date( "U", strtotime( '2021-10-29 22:29:00' ) ),
  *
- *         'currency'        => 'USD', // 3-letter currency code. If not provided, defaults according to your settings.
+ *                           // Optional. 3-letter currency code. Defaults according to your settings.
+ *         'currency'        => 'USD',
  *
- *                           // UNIX timestamp to be used for due date. If not provided, defaults according to your settings:
+ *                           // Optional. Content of "Description" field.
+ *         'description'     => 'Description of work to be provided to John Doe.',
+ *
+ *                           // Optional. A discount amount to apply to the invoice. Default is 0.
+ *         'discount'        => 5,
+ *
+ *                           // Optional. UNIX timestamp to be used for due date. Defaults according to your settings.
  *         'due_date'        => date( "U", strtotime( '2021-11-29 22:29:00' ) ),
  *
- *                           // the number of the invoice. If not provided, defaults to next available number according to your settings.
+ *                           // Optional. The invoice number. Defaults to next available number according to your settings.
  *         'number'          => 123456,
  *
- *                           // prefix to the invoice number. If not provided, defaults according to your settings.
+ *                           // Optional. A prefix to the invoice number. Defaults according to your settings.
  *         'number_prefix'   => 'INV-',
  *
- *                           // suffix to the invoice number. If not provided, defaults according to your settings.
+ *                           // Optional. A suffix to the invoice number. Defaults according to your settings.
  *         'number_suffix'   => '-web',
  *
  *                           // REQUIRED. the line items of the invoice, in the following format:
@@ -49,16 +55,29 @@ if ( ! defined( 'ABSPATH' ) ) {
  *                                      'amount'  => '20',
  *                                      'taxable' => false, // not taxable
  *                                  ),
- *                              );
+ *                              ),
  *
- *                           // array of payment methods to enable for this invoice. If not provided, defaults to all available
+ *                           // Optional. Array of payment methods to enable for this invoice. Defaults to all available
  *                           // payment methods according to your settings.
  *         'payment_methods' => array( 'generic', 'bank', 'paypal' ),
  *
- *         'status'          => 'unpaid', // the status of the invoice. If not provided, defaults to 'draft'.
+ *                           // Optional. The status of the invoice. Defaults to 'draft'.
+ *         'status'          => 'unpaid',
  *
- *         'terms'           => 'Net 30 days.', // the invoice terms. If not provided, defaults according to your settings.
- *     );
+ *                           // Optional. Tax rate, expressed as percentage. Defaults according to your settings.
+ *         'tax'             => '9.5',
+ *
+ *                           // Optional. Tax calculation method ('inclusive' or 'exclusive'). Defaults according to your settings.
+ *         'tax_calc_method' => 'exclusive',
+ *
+ *                           // Optional. Tax name. Defaults according to your settings.
+ *         'tax_name'        => 'Tax',
+ *
+ *                           // Optional. The invoice terms. Defaults according to your settings.
+ *         'terms'           => 'Net 30 days.',
+ *
+ *                           // Optional. The invoice title.
+ *         'title'           => 'Invoice for John Doe',
  *
  * @version 3.x.x
  * @since   3.x.x
@@ -129,12 +148,28 @@ function sliced_invoices_create_invoice( $args ) {
 		isset( $args['currency'] ) ? $args['currency'] : 'default'
 	);
 	
+	// discount:
+	if ( isset( $args['discount'] ) ) {
+		update_post_meta( $invoice_id, 'sliced_invoice_discount', $args['discount'] );
+	}
+	
 	// automatically enable your chosen payment methods:
 	update_post_meta(
 		$invoice_id,
 		'_sliced_payment_methods',
 		isset( $args['payment_methods'] ) ? $args['payment_methods'] : array_keys( sliced_get_accepted_payment_methods() )
 	);
+	
+	// tax settings: (if not set, invoice will default to global settings)
+	if ( isset( $args['tax'] ) ) {
+		update_post_meta( $invoice_id, '_sliced_tax', $args['tax'] );
+	}
+	if ( isset( $args['tax_calc_method'] ) ) {
+		update_post_meta( $invoice_id, '_sliced_tax_calc_method', $args['tax_calc_method'] );
+	}
+	if ( isset( $args['tax_name'] ) ) {
+		update_post_meta( $invoice_id, '_sliced_tax_name', $args['tax_name'] );
+	}
 	
 	// invoice terms:
 	update_post_meta(
@@ -169,29 +204,32 @@ function sliced_invoices_create_invoice( $args ) {
  * all others are optional.  Example $args:
  *
  *     $args = array(
- *         'title'           => 'Quote for John Doe',
  *
- *         'description'     => 'Description of work to be provided to John Doe.',
+ *                           // REQUIRED. the WordPress User ID corresponding to the Sliced Client record.
+ *         'client_id'       => 123,
  *
- *         'client_id'       => 123, // REQUIRED. the WordPress User ID corresponding to the Sliced Client record.
+ *                           // Optional. The WordPress User ID of the person creating the quote. Defaults to the current user.
+ *         'creator_user_id' => 1,
  *
- *         'creator_user_id' => 1,   // the WordPress User ID of the person creating the quote.
- *
- *                           // UNIX timestamp to be used for quote date. If not provided, defaults to now:
+ *                           // Optional. UNIX timestamp to be used for quote date. Defaults to current time.
  *         'created_at'      => date( "U", strtotime( '2021-10-29 22:29:00' ) ),
  *
- *         'currency'        => 'USD', // 3-letter currency code. If not provided, defaults according to your settings.
+ *                           // Optional. 3-letter currency code. Defaults according to your settings.
+ *         'currency'        => 'USD',
  *
- *                           // UNIX timestamp to be used for the "valid until" date. If not provided, defaults according to your settings:
- *         'valid_until'     => date( "U", strtotime( '2021-11-29 22:29:00' ) ),
+ *                           // Optional. Content of "Description" field.
+ *         'description'     => 'Description of work to be provided to John Doe.',
  *
- *                           // the number of the quote. If not provided, defaults to next available number according to your settings.
+ *                           // Optional. A discount amount to apply to the quote. Default is 0.
+ *         'discount'        => 5,
+ *
+ *                           // Optional. The quote number. Defaults to next available number according to your settings.
  *         'number'          => 123456,
  *
- *                           // prefix to the quote number. If not provided, defaults according to your settings.
+ *                           // Optional. A prefix to the quote number. Defaults according to your settings.
  *         'number_prefix'   => 'QUO-',
  *
- *                           // suffix to the quote number. If not provided, defaults according to your settings.
+ *                           // Optional. A suffix to the quote number. Defaults according to your settings.
  *         'number_suffix'   => '-web',
  *
  *                           // REQUIRED. the line items of the quote, in the following format:
@@ -208,11 +246,29 @@ function sliced_invoices_create_invoice( $args ) {
  *                                      'amount'  => '20',
  *                                      'taxable' => false, // not taxable
  *                                  ),
- *                              );
+ *                              ),
  *
- *         'status'          => 'sent', // the status of the quote. If not provided, defaults to 'draft'.
+ *                           // Optional. The status of the quote. Defaults to 'draft'.
+ *         'status'          => 'sent',
  *
- *         'terms'           => 'Net 30 days.', // the quote terms. If not provided, defaults according to your settings.
+ *                           // Optional. Tax rate, expressed as percentage. Defaults according to your settings.
+ *         'tax'             => '9.5',
+ *
+ *                           // Optional. Tax calculation method ('inclusive' or 'exclusive'). Defaults according to your settings.
+ *         'tax_calc_method' => 'exclusive',
+ *
+ *                           // Optional. Tax name. Defaults according to your settings.
+ *         'tax_name'        => 'Tax',
+ *
+ *                           // Optional. The quote terms. Defaults according to your settings.
+ *         'terms'           => 'Net 30 days.',
+ *
+ *                           // Optional. The quote title.
+ *         'title'           => 'Quote for John Doe',
+ *
+ *                           // UNIX timestamp to be used for the "valid until" date. Defaults according to your settings.
+ *         'valid_until'     => date( "U", strtotime( '2021-11-29 22:29:00' ) ),
+ *
  *     );
  *
  * @version 3.x.x
@@ -283,6 +339,22 @@ function sliced_invoices_create_quote( $args ) {
 		'_sliced_currency',
 		isset( $args['currency'] ) ? $args['currency'] : 'default'
 	);
+	
+	// discount:
+	if ( isset( $args['discount'] ) ) {
+		update_post_meta( $quote_id, 'sliced_invoice_discount', $args['discount'] );
+	}
+	
+	// tax settings: (if not set, quote will default to global settings)
+	if ( isset( $args['tax'] ) ) {
+		update_post_meta( $quote_id, '_sliced_tax', $args['tax'] );
+	}
+	if ( isset( $args['tax_calc_method'] ) ) {
+		update_post_meta( $quote_id, '_sliced_tax_calc_method', $args['tax_calc_method'] );
+	}
+	if ( isset( $args['tax_name'] ) ) {
+		update_post_meta( $quote_id, '_sliced_tax_name', $args['tax_name'] );
+	}
 	
 	// quote terms:
 	update_post_meta(
